08475f3fbbe5872dff3156bbf02c83e41ba71aec
Brandon Teich <brandonteich123@gmail.com>
2018-12-24 19:23:36 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,18 @@
 require 'open-uri'
 require 'pry'
-
+require 'nokogiri'
 class Scraper
-
+  
+  
   def self.scrape_index_page(index_url)
-    
+        emptyHashArray =[]
+        open(index_url)
+        doc = Nokogiri::HTML(open(index_url))    
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+       Nokogiri::HTML(open(profile_url))
   end
 
 end
